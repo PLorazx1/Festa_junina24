55941674d93451d4ec606b08f26f6872292748a6
Paulo Loraschi <paulo.ss.loraschi@gmail.com>
2024-05-20 00:52:44 -0300

    inicialização da back-end

--- comandos.js
+++ comandos.js
@@ -1,7 +1,30 @@
 var produto, preco, quantidade;
+var pedidos = [];
+/* TENTATIVA POR ID
+function testing(id){
+    
+    var teste = document.querySelector(`#${id}`);
+    alert(id);
+    
+};*/
 
-var teste = document.getElementById('Comidas');
+//TENTATIVA POR LISTAS
+const valores = [2.0, 3.5, 4];
+const produtos = ['tacaca', 'vatapa', 'bolo'];
 
-teste.onclick = function(){
-    alert(this.id);
-}
+function comprar(cod){
+    preco = valores[cod];
+    produto = produtos[cod];
+    window.location.href = 'confirmacao.html';
+};
+
+function cancelar(){
+    preco = undefined;
+    produto = undefined;
+    window.location.href = 'index.html';
+};
+
+function confirmar(){
+    quantidade = document.querySelector('input_quant');
+    console.log(produto);
+};
